c9ce3e40efca96e69e983d570482625d15885f2d
clearfeld <clearfeld@outlook.com>
2020-09-15 23:11:43 -0400

    Update: paint functions to use theme id instead of hard coded value

--- packages/paint_functions/paint_functions.cpp
+++ packages/paint_functions/paint_functions.cpp
@@ -1,12 +1,14 @@
 // Credit to Skytrias
 // https://github.com/Skytrias/4files/blob/e254e55d9c310eeaa7f77d6a377ba517d6c1f055/render_highlight.cpp#L462
 
+CUSTOM_ID(colors, internal_package__paint_functions);
+
 function void
 st_paint_functions(Application_Links *app, Buffer_ID buffer, Text_Layout_ID text_layout_id) {
     i64 keyword_length = 0;
     i64 start_pos = 0;
     i64 end_pos = 0;
-
+    
 	Token_Array array = get_token_array_from_buffer(app, buffer);
     if (array.tokens != 0){
         Range_i64 visible_range = text_layout_get_visible_range(app, text_layout_id);
@@ -17,38 +19,38 @@ st_paint_functions(Application_Links *app, Buffer_ID buffer, Text_Layout_ID text
             if (token->pos >= visible_range.one_past_last){
                 break;
             }
-
+            
             // get pos at paren
             // NOTE(Skytrias): use token->sub_kind == TokenCppKind_ParenOp if only '(' should be used
 			if (keyword_length != 0 && token->kind == TokenBaseKind_ParentheticalOpen) {
                 end_pos = token->pos;
             }
-
+            
             // search for default text, count up the size
             if (token->kind == TokenBaseKind_Identifier) {
                 if (keyword_length == 0) {
                     start_pos = token->pos;
                 }
-
+                
                 keyword_length += 1;
             } else {
                 keyword_length = 0;
             }
-
+            
             // color text
             if (start_pos != 0 && end_pos != 0) {
                 Range_i64 range = { 0 };
                 range.start = start_pos;
                 range.end = end_pos;
-
+                
 				// NOTE(Skytrias): use your own colorscheme her via fcolor_id(defcolor_*)
 				// NOTE(Skytrias): or set the color you'd like to use globally like i do
-                paint_text_color(app, text_layout_id, range, 0xFF7EBEBD);
-
+                paint_text_color(app, text_layout_id, range, fcolor_resolve(fcolor_id(internal_package__paint_functions)));
+                
                 end_pos = 0;
                 start_pos = 0;
             }
-
+            
             if (!token_it_inc_all(&it)){
                 break;
             }
